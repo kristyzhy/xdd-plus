d4480aa2fc5fcaa3d6cc140c1ffc5ba422e1a568
kristyzhy <kristyzhy0409@qq.com>
2021-09-15 00:09:23 +0800

    Update config.go

--- models/config.go
+++ models/config.go
@@ -31,6 +31,8 @@ type Yaml struct {
 	QbotPublicMode      bool   `yaml:"qbot_public_mode"`
 	DailyAssetPushCron  string `yaml:"daily_asset_push_cron"`
 	Version             string `yaml:"version"`
+	CTime               string `yaml:"AtTime"`
+	IsHelp              bool   `yaml:"IsHelp"`
 	Node                string
 	Npm                 string
 	Python              string
@@ -80,6 +82,9 @@ func initConfig() {
 	}
 	if Config.Master == "" {
 		Config.Master = "xxxx"
+	}
+	if Config.CTime == "" {
+		Config.CTime = "10"
 	}
 	if Config.Mode != Parallel {
 		Config.Mode = Balance
