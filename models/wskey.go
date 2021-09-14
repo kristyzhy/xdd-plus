33fa42a9061a9f71a54e0216b84e3e013b55aa57
kristyzhy <kristyzhy0409@qq.com>
2021-09-15 00:20:21 +0800

    Update wskey.go

--- models/wskey.go
+++ models/wskey.go
@@ -2,14 +2,18 @@ package models
 
 import (
 	"fmt"
+	"github.com/beego/beego/v2/core/logs"
 	"github.com/robfig/cron/v3"
+	"math/rand"
+	"strconv"
 )
 
 func intiSky() {
 	c := cron.New(cron.WithSeconds()) //精确到秒
 
 	//定时任务
-	spec := "0 0 12,23 * * ?" //cron表达式，每秒一次
+	spec := "0 " + strconv.Itoa(rand.Intn(59)) + " " + Config.CTime + "/12 * * ?" //cron表达式，每秒一次
+	logs.Info(spec)
 	c.AddFunc(spec, func() {
 		fmt.Println("开始wskey转换")
 		updateCookie()
