9ffff82662b4f64b39e626d18f7ba75fa8d1758c
kristyzhy <kristyzhy0409@qq.com>
2021-09-16 10:30:10 +0800

    Update asset.go

--- models/asset.go
+++ models/asset.go
@@ -72,7 +72,8 @@ func (ck *JdCookie) Query1() string {
 	//log.Info(msg)
 	if !strings.Contains(msg,"cookies"){
 		msg=regexp.MustCompile(`^(.+\s+){3}|\s*.+\s*$|.*东东工厂.*\s*`).ReplaceAllString(msg,"")
-		msg=fmt.Sprintf("账号昵称：%s\n绑定QQ: %v\n用户名：%s\n用户等级：%v\n等级名称：%v\n更新时间: %s\n%s",ck.Nickname,ck.QQ,ck.PtPin,ck.UserLevel,ck.LevelName,ck.CreateAt,msg)
+		msg=fmt.Sprintf("账号昵称：%s\n绑定QQ: %v\n用户名：%s\n用户等级：%v\n等级名称：%v\n%s",ck.Nickname,ck.QQ,ck.PtPin,ck.UserLevel,ck.LevelName,msg)
+	
 	}else if CookieOK(ck){
 		msg=fmt.Sprintf("查询失败\n账号: %s\n备注: %s\n%s",ck.PtPin,ck.Note,msg)
 	}else{
@@ -90,7 +91,6 @@ func (ck *JdCookie) Query() string {
 	}
 	asset := Asset{}
 	if CookieOK(ck) {
-		msgs = append(msgs, fmt.Sprintf("绑定QQ：%v", ck.QQ))
 		msgs = append(msgs, fmt.Sprintf("用户名：%v", ck.PtPin))
 		msgs = append(msgs, fmt.Sprintf("用户等级：%v", ck.UserLevel))
 		msgs = append(msgs, fmt.Sprintf("等级名称：%v", ck.LevelName))
